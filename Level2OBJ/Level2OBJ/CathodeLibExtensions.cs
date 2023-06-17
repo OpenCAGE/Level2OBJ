8b4928edb479af7b325ba06009aeb18586be3167
MattFiler <matthew.filer@hotmail.co.uk>
2023-06-17 10:37:41 +0100

    initial sorta working output

---
+++ Level2OBJ/Level2OBJ/CathodeLibExtensions.cs
@@ -0,0 +1,180 @@
+﻿using Assimp;
+using CathodeLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using static CATHODE.Models;
+
+namespace Level2OBJ
+{
+    public static class CathodeLibExtensions
+    {
+        public static Mesh ToMesh(this CS2.Component.LOD.Submesh submesh)
+        {
+            Mesh mesh = new Mesh();
+            mesh.MaterialIndex = 0; //todo
+
+            List<int> indices = new List<int>();
+            List<Vector3> vertices = new List<Vector3>();
+            List<Vector3> normals = new List<Vector3>();
+            List<Vector4> tangents = new List<Vector4>();
+            List<Vector2> uv0 = new List<Vector2>();
+            List<Vector2> uv1 = new List<Vector2>();
+            List<Vector2> uv2 = new List<Vector2>();
+            List<Vector2> uv3 = new List<Vector2>();
+            List<Vector2> uv7 = new List<Vector2>();
+
+            //TODO: implement skeleton lookup for the indexes
+            List<Vector4> boneIndex = new List<Vector4>(); //The indexes of 4 bones that affect each vertex
+            List<Vector4> boneWeight = new List<Vector4>(); //The weights for each bone
+
+            if (submesh.content.Length == 0) return mesh;
+
+            using (BinaryReader reader = new BinaryReader(new MemoryStream(submesh.content)))
+            {
+                for (int i = 0; i < submesh.VertexFormat.Elements.Count; ++i)
+                {
+                    if (i == submesh.VertexFormat.Elements.Count - 1)
+                    {
+                        for (int x = 0; x < submesh.IndexCount; x++)
+                            indices.Add(reader.ReadUInt16());
+
+                        continue;
+                    }
+
+                    for (int x = 0; x < submesh.VertexCount; ++x)
+                    {
+                        for (int y = 0; y < submesh.VertexFormat.Elements[i].Count; ++y)
+                        {
+                            AlienVBF.Element format = submesh.VertexFormat.Elements[i][y];
+                            switch (format.VariableType)
+                            {
+                                case VBFE_InputType.VECTOR3:
+                                    {
+                                        Vector3 v = new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+                                        switch (format.ShaderSlot)
+                                        {
+                                            case VBFE_InputSlot.NORMAL:
+                                                normals.Add(v);
+                                                break;
+                                            case VBFE_InputSlot.TANGENT:
+                                                tangents.Add(new Vector4((float)v.X, (float)v.Y, (float)v.Z, 0));
+                                                break;
+                                            case VBFE_InputSlot.UV:
+                                                //TODO: 3D UVW
+                                                break;
+                                        };
+                                        break;
+                                    }
+                                case VBFE_InputType.INT32:
+                                    {
+                                        int v = reader.ReadInt32();
+                                        switch (format.ShaderSlot)
+                                        {
+                                            case VBFE_InputSlot.COLOUR:
+                                                //??
+                                                break;
+                                        }
+                                        break;
+                                    }
+                                case VBFE_InputType.VECTOR4_BYTE:
+                                    {
+                                        Vector4 v = new Vector4(reader.ReadByte(), reader.ReadByte(), reader.ReadByte(), reader.ReadByte());
+                                        switch (format.ShaderSlot)
+                                        {
+                                            case VBFE_InputSlot.BONE_INDICES:
+                                                boneIndex.Add(v);
+                                                break;
+                                        }
+                                        break;
+                                    }
+                                case VBFE_InputType.VECTOR4_BYTE_DIV255:
+                                    {
+                                        Vector4 v = new Vector4(reader.ReadByte(), reader.ReadByte(), reader.ReadByte(), reader.ReadByte());
+                                        v /= 255.0f;
+                                        switch (format.ShaderSlot)
+                                        {
+                                            case VBFE_InputSlot.BONE_WEIGHTS:
+                                                boneWeight.Add(v / (v.X + v.Y + v.Z + v.W));
+                                                break;
+                                            case VBFE_InputSlot.UV:
+                                                uv2.Add(new Vector2(v.X, v.Y));
+                                                uv3.Add(new Vector2(v.Z, v.W));
+                                                break;
+                                        }
+                                        break;
+                                    }
+                                case VBFE_InputType.VECTOR2_INT16_DIV2048:
+                                    {
+                                        Vector2 v = new Vector2(reader.ReadInt16() / 2048.0f, reader.ReadInt16() / 2048.0f);
+                                        switch (format.ShaderSlot)
+                                        {
+                                            case VBFE_InputSlot.UV:
+                                                if (format.VariantIndex == 0) uv0.Add(v);
+                                                else if (format.VariantIndex == 1)
+                                                {
+                                                    // TODO: We can figure this out based on AlienVBFE.
+                                                    //Material->Material.Flags |= Material_HasTexCoord1;
+                                                    uv1.Add(v);
+                                                }
+                                                else if (format.VariantIndex == 2) uv2.Add(v);
+                                                else if (format.VariantIndex == 3) uv3.Add(v);
+                                                else if (format.VariantIndex == 7) uv7.Add(v);
+                                                break;
+                                        }
+                                        break;
+                                    }
+                                case VBFE_InputType.VECTOR4_INT16_DIVMAX:
+                                    {
+                                        Vector4 v = new Vector4(reader.ReadInt16(), reader.ReadInt16(), reader.ReadInt16(), reader.ReadInt16());
+                                        v /= (float)Int16.MaxValue;
+                                        if (v.W != 0 && v.W != -1 && v.W != 1) throw new Exception("Unexpected vert W");
+                                        v *= submesh.ScaleFactor; //Account for scale
+                                        switch (format.ShaderSlot)
+                                        {
+                                            case VBFE_InputSlot.VERTEX:
+                                                vertices.Add(new Vector3(v.X, v.Y, v.Z));
+                                                break;
+                                        }
+                                        break;
+                                    }
+                                case VBFE_InputType.VECTOR4_BYTE_NORM:
+                                    {
+                                        Vector4 v = new Vector4(reader.ReadByte(), reader.ReadByte(), reader.ReadByte(), reader.ReadByte());
+                                        v /= (float)byte.MaxValue - 0.5f;
+                                        v = Vector4.Normalize(v);
+                                        switch (format.ShaderSlot)
+                                        {
+                                            case VBFE_InputSlot.NORMAL:
+                                                normals.Add(new Vector3(v.X, v.Y, v.Z));
+                                                break;
+                                            case VBFE_InputSlot.TANGENT:
+                                                break;
+                                            case VBFE_InputSlot.BITANGENT:
+                                                break;
+                                        }
+                                        break;
+                                    }
+                            }
+                        }
+                    }
+                    Utilities.Align(reader, 16);
+                }
+            }
+
+            if (vertices.Count == 0) return mesh;
+
+            for (int i = 0; i < vertices.Count; i++)
+                mesh.Vertices.Add(new Assimp.Vector3D((float)vertices[i].X, (float)vertices[i].Y, (float)vertices[i].Z));
+            mesh.HasTextureCoords(0);
+            for (int i = 0; i < uv0.Count; i++)
+                mesh.TextureCoordinateChannels[0].Add(new Assimp.Vector3D((float)uv0[i].X, (float)uv0[i].Y, 0));
+            for (int i = 0; i < normals.Count; i++)
+                mesh.Normals.Add(new Assimp.Vector3D((float)normals[i].X, (float)normals[i].Y, (float)normals[i].Z));
+            bool worked = mesh.SetIndices(indices.ToArray(), 3);
+            if (!worked) throw new Exception("oops");
+            return mesh;
+        }
+    }
+}
--- Level2OBJ/Level2OBJ/Program.cs
+++ Level2OBJ/Level2OBJ/Program.cs
@@ -1,8 +1,10 @@
-﻿using CATHODE;
+﻿using Assimp;
+using CATHODE;
 using CATHODE.Scripting;
 using CATHODE.Scripting.Internal;
 using System;
-using System.Numerics;
+using System.IO;
+using static CATHODE.Models;
 
 namespace Level2OBJ
 {
@@ -11,6 +13,8 @@ namespace Level2OBJ
         static Commands commands;
         static RenderableElements reds;
         static Models models;
+
+        static Scene scene;
 
         [STAThread]
         static void Main(string[] args)
@@ -21,33 +25,48 @@ namespace Level2OBJ
             reds = new RenderableElements(level + "/WORLD/REDS.BIN");
             models = new Models(level + "/RENDERABLE/LEVEL_MODELS.PAK");
 
-            ParseComposite(commands.EntryPoints[0], Matrix4x4.Identity);
+            scene = new Scene();
+            scene.Materials.Add(new Assimp.Material());
+            scene.RootNode = new Node(level);
+
+            ParseComposite(commands.EntryPoints[0], scene.RootNode);
+
+            AssimpContext exp = new AssimpContext();
+            exp.ExportFile(scene, "out.obj", "obj");
+            exp.Dispose();
 
             Console.WriteLine("Done!");
             Console.ReadLine();
         }
 
-        static void ParseComposite(Composite composite, Matrix4x4 stackedTransform)
+        static void ParseComposite(Composite composite, Node node)
         {
             if (composite == null) return;
-            Console.WriteLine("Parsing: " + composite.name);
 
             foreach (OverrideEntity ovrride in composite.overrides)
             {
                 //Store all overrides here so that we respect them as we continue down the hierarchy
             }
+
             foreach (FunctionEntity function in composite.functions)
             {
                 if (!CommandsUtils.FunctionTypeExists(function.function))
                 {
-                    //Continue down the hierarchy
-                    Matrix4x4 transform = Matrix4x4.Add(stackedTransform, GetMatrix(function)); //need to respect overrides here
-                    ParseComposite(commands.GetComposite(function.function), transform);
+                    Composite compositeNext = commands.GetComposite(function.function);
+                    if (compositeNext != null)
+                    {
+                        Node nodeNext = new Node(compositeNext.name);
+                        nodeNext.Transform = GetMatrix(function); //need to respect overrides here
+                        node.Children.Add(nodeNext);
+                        ParseComposite(compositeNext, nodeNext);
+                    }
                 }
                 else if (CommandsUtils.GetFunctionType(function.function) == FunctionType.ModelReference)
                 {
-                    //Store model info
-                    Matrix4x4 transform = Matrix4x4.Add(stackedTransform, GetMatrix(function)); //need to respect overrides here
+                    Node nodeModel = new Node();
+                    nodeModel.Transform = GetMatrix(function); //need to respect overrides here
+                    node.Children.Add(nodeModel);
+
                     Parameter resourceParam = function.GetParameter("resource");
                     if (resourceParam != null && resourceParam.content != null)
                     {
@@ -61,10 +80,11 @@ namespace Level2OBJ
                                     for (int i = 0; i < resourceRef.count; i++)
                                     {
                                         RenderableElements.Element renderable = reds.Entries[resourceRef.startIndex + i];
-                                        Models.CS2.Component.LOD.Submesh mesh = models.GetAtWriteIndex(renderable.ModelIndex);
+                                        Models.CS2.Component.LOD.Submesh submesh = models.GetAtWriteIndex(renderable.ModelIndex);
 
-                                        Console.WriteLine("\tFound model: " + models.FindModelForSubmesh(mesh)?.Name + " (" + transform.ToString() + ")");
-                                        break;
+                                        Mesh mesh = submesh.ToMesh();
+                                        scene.Meshes.Add(mesh);
+                                        nodeModel.MeshIndices.Add(scene.Meshes.Count - 1);
                                     }
                                 }
                                 break;
@@ -83,9 +103,10 @@ namespace Level2OBJ
                 {
                     case DataType.TRANSFORM:
                         cTransform transform = (cTransform)positionParam.content;
-                        Matrix4x4 position = Matrix4x4.CreateTranslation(transform.position);
-                        Matrix4x4 rotation = Matrix4x4.CreateFromYawPitchRoll(transform.rotation.X, transform.rotation.Y, transform.rotation.Z); //todo: this may be flipped
-                        return Matrix4x4.Add(position, rotation);
+                        Matrix4x4 position = Matrix4x4.FromTranslation(new Vector3D(transform.position.X, transform.position.Y, transform.position.Z));
+                        Matrix4x4 rotation = Matrix4x4.FromEulerAnglesXYZ(new Vector3D(transform.rotation.X, transform.rotation.Y, transform.rotation.Z));
+                        Matrix4x4 scale = Matrix4x4.FromScaling(new Vector3D(1, 1, 1));
+                        return position * rotation * scale;
                 }
             }
             return Matrix4x4.Identity;
