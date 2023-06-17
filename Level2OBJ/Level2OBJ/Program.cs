a34135ca0648e4a938e49038e29a45b7c62efcb7
MattFiler <matthew.filer@hotmail.co.uk>
2023-06-17 12:26:18 +0100

    slightly better transforms

--- Level2OBJ/Level2OBJ/Program.cs
+++ Level2OBJ/Level2OBJ/Program.cs
@@ -44,7 +44,7 @@ namespace Level2OBJ
             ParseComposite(commands.EntryPoints[0], scene.RootNode);
 
             AssimpContext exp = new AssimpContext();
-            exp.ExportFile(scene, "out.obj", "obj");
+            exp.ExportFile(scene, "out.fbx", "fbx");
             exp.Dispose();
 
             Console.WriteLine("Done!");
@@ -68,7 +68,7 @@ namespace Level2OBJ
                     if (compositeNext != null)
                     {
                         Node nodeNext = new Node(compositeNext.name);
-                        nodeNext.Transform = GetMatrix(function); //need to respect overrides here
+                        nodeNext.Transform = GetEntityMatrix(function); //need to respect overrides here
                         node.Children.Add(nodeNext);
                         ParseComposite(compositeNext, nodeNext);
                     }
@@ -76,7 +76,7 @@ namespace Level2OBJ
                 else if (CommandsUtils.GetFunctionType(function.function) == FunctionType.ModelReference)
                 {
                     Node nodeModel = new Node();
-                    nodeModel.Transform = GetMatrix(function); //need to respect overrides here
+                    nodeModel.Transform = GetEntityMatrix(function); //need to respect overrides here
                     node.Children.Add(nodeModel);
 
                     Parameter resourceParam = function.GetParameter("resource");
@@ -88,12 +88,16 @@ namespace Level2OBJ
                                 cResource resource = (cResource)resourceParam.content;
                                 foreach (ResourceReference resourceRef in resource.value)
                                 {
-                                    //TODO: really, we should load collision meshes rather than renderables?
                                     if (resourceRef.entryType != ResourceType.RENDERABLE_INSTANCE) continue;
+
+                                    Node nodeModelPart = new Node();
+                                    nodeModelPart.Transform = ToMatrix(resourceRef.position, resourceRef.rotation);
+                                    nodeModel.Children.Add(nodeModelPart);
+
                                     for (int i = 0; i < resourceRef.count; i++)
                                     {
                                         RenderableElements.Element renderable = reds.Entries[resourceRef.startIndex + i];
-                                        nodeModel.MeshIndices.Add(renderable.ModelIndex);
+                                        nodeModelPart.MeshIndices.Add(renderable.ModelIndex);
                                     }
                                 }
                                 break;
@@ -103,7 +107,7 @@ namespace Level2OBJ
             }
         }
 
-        static Matrix4x4 GetMatrix(Entity entity)
+        static Matrix4x4 GetEntityMatrix(Entity entity)
         {
             Parameter positionParam = entity.GetParameter("position");
             if (positionParam != null && positionParam.content != null)
@@ -112,14 +116,18 @@ namespace Level2OBJ
                 {
                     case DataType.TRANSFORM:
                         cTransform transform = (cTransform)positionParam.content;
-                        Matrix4x4 position = Matrix4x4.FromTranslation(new Vector3D(transform.position.X, transform.position.Y, transform.position.Z));
-                        //Matrix4x4 rotation = Matrix4x4.FromEulerAnglesXYZ(transform.rotation.X * PI / 180.0f * -1.0f, transform.rotation.Y * PI / 180.0f * -1.0f, transform.rotation.Z * PI / 180.0f * -1.0f);
-                        Matrix4x4 rotation = Matrix4x4.Identity; //TODO: why is the above not correct?
-                        Matrix4x4 scale = Matrix4x4.FromScaling(new Vector3D(1, 1, 1));
-                        return position * rotation * scale;
+                        return ToMatrix(transform.position, transform.rotation);
                 }
             }
             return Matrix4x4.Identity;
+        }
+
+        static Matrix4x4 ToMatrix(System.Numerics.Vector3 position, System.Numerics.Vector3 rotation)
+        {
+            Matrix4x4 positionM = Matrix4x4.FromTranslation(new Vector3D(position.X, position.Y, position.Z));
+            Matrix4x4 rotationM = Matrix4x4.FromEulerAnglesXYZ(PI * rotation.X / 180.0f, PI * rotation.Y / 180.0f, PI * rotation.Z / 180.0f);
+            Matrix4x4 scaleM = Matrix4x4.FromScaling(new Vector3D(1, 1, 1));
+            return scaleM * rotationM * positionM;
         }
     }
 }
